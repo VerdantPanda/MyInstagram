4e50ad072450a7a8ef36b983b71cce24cb2cac2f
Ibrahim Fagbamila <ibrahimf@fb.com>
2019-07-11 16:24:01 -0700

    User can now create new account.

--- app/src/main/java/com/example/myinstagram/MainActivity.java
+++ app/src/main/java/com/example/myinstagram/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     EditText etPasswordInput;
     @BindView(R.id.btLogin)
     Button btLogin;
+    @BindView(R.id.btSignUp)
+    Button btSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "LOGIN ATTEMPT OCCURED", Toast.LENGTH_LONG).show();
                 final String username = etUsernameInput.getText().toString();
                 final String password = etPasswordInput.getText().toString();
                 login(username, password);
@@ -44,13 +48,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // user persist
-        ParseUser currentUser = ParseUser.getCurrentUser();
-        if (currentUser != null) {
-            Intent toHomeActivity = new Intent(MainActivity.this, HomeActivity.class);
-            startActivity(toHomeActivity);
-            finish();
-        }
+
+        btSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "sign up clicked", Toast.LENGTH_LONG).show();
+                Intent toSignUpActivity = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(toSignUpActivity);
+                final String username = etUsernameInput.getText().toString();
+                final String password = etPasswordInput.getText().toString();
+                //registerUser(username, password);
+
+
+            }
+        });
+
+
+//        // user persist
+//        ParseUser currentUser = ParseUser.getCurrentUser();
+//        if (currentUser != null) {
+//            Intent toHomeActivity = new Intent(MainActivity.this, HomeActivity.class);
+//            startActivity(toHomeActivity);
+//            finish();
+//        }
 
     }
 
@@ -75,11 +96,32 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Login Failed. Try Again!", Toast.LENGTH_LONG).show();
                     Log.d("LoginActivity", "Failed");
                     Log.d("LoginActivity", e.toString());
-                    ;
+
                 }
             }
         });
     }
 
+    private void registerUser(String username, String password){
+        Toast.makeText(getApplicationContext(), "Sign up 2 clicked", Toast.LENGTH_LONG).show();
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        //user.setEmail(etEmail.getText().toString());
+        user.put("handle", username);
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    login(etUsernameInput.getText().toString(), etPasswordInput.getText().toString());
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+
+                    Toast.makeText(getApplicationContext(), "Sign up FAILED", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
 }
